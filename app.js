706d29591e985fa9eef4f0c8263e71a840e7fbba
AnsHPaTeL2927 <patelansh2918@gmail.com>
2023-11-02 08:15:33 +0530

    host API FoodCourt

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 // import cors from "cors";
-var cors = require('cors')
+const cors = require('cors')
 const app = express();
 const connectDB = require('./DB/connect')
 const PORT = process.env.PORT || 5000;
@@ -12,12 +12,12 @@ app.get("/api/restaurants", (res, req) => {
     res.setEncoding("Hi, I am Live");
 });
 
-const corsOptions = {
-    origin: true,
-  };
 app.use("/", restaurant_routes);  
-app.use(cors(corsOptions)); 
-app.use(cors({ origin: 'http://localhost:3000/' }));
+
+app.use(
+  cors({ 
+    origin: 'http://localhost:3000',   
+  }));
 
  
 const start = async () => {

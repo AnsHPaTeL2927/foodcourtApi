8457ffaeb1f27d065234a3ff2531ad69f3af061c
AnsHPaTeL2927 <patelansh2918@gmail.com>
2023-11-08 15:24:17 +0530

    host API FoodCourt

--- controllers/restaunrant.js
+++ controllers/restaunrant.js
@@ -1,7 +1,15 @@
 const restaurant = require("../models/restaurant_schema");
 
 const getAllRestaurant = async (req, res) => {
-    const myData = await restaurant.find(req.query);
+    const {_id} = req.query;
+    const queryObject = {};
+
+    if(_id){
+        queryObject._id = _id;
+    }
+
+    let apiData = restaurant.find(queryObject);
+    const myData = await apiData;
     res.status(200).json({myData});
 };
 const getAllRestaurantTesting = async (req, res) => {
